e31debb1c55eeaf62673d1919bb48e77e7480bd6
Gui2908 <guilherme2908@hotmail.com>
2018-10-05 10:42:49 -0700

    added Header to Components, created login page, need to fix routes to play game after logged in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import AuthService from './services'
 import Register from './pages/Register.js';
 import Home from './pages/Home.js';
 import Game from './pages/Game.js';
+import Login from './pages/Login.js';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
+  //new instance of AuthService
+  let auth = new AuthService()
+
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/game' component={Game}/>
-            <Route path='/register' component={Register}/>
+          {(auth.loggedIn())
+          //if logged in
+          ? <Switch>
+            <Route exact path='/' component={Home} / >
+            <Route exact path='/game' component={Game} / >
           </Switch>
+          //if not logged in
+          : <Switch>
+            <Route exact path='/login' component={Login} / >
+            <Route exact path='/register' component={Register} / >
+            <Redirect from='/game' to='/Login' / >
+          </Switch>
+        }
         </Router>
       </div>
     );
