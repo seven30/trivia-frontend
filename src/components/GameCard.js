6d56d09e1bd5a5875c05b9006001f51c36b28423
Madison Grant <madisonrgrant@gmail.com>
2018-10-08 14:57:29 -0700

    Homepade edited, Header changed into LoginButton, guest able to play game without logging in.

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -3,7 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import '../pages/Game.css';
+
 import { shuffle, replaceUnicode } from '../helper_functions/helper-functions.js';
 
 class GameCard extends Component {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import './Home.css';
-import Header from '../components/Header'
+import Logo from '../components/TriviaNightLogo.png';
+import LoginButton from '../components/LoginButton';
+
+import { Link } from 'react-router-dom'
+
 import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+const theme = createMuiTheme({
 
+  palette: {
+    primary: {main: '#000000'},
+    secondary: {main: '#AED6F1'},
+    type: 'dark',
+  }
+})
+
+      // <div style={{ padding: "10vh" }}>
 class Home extends Component {
   render() {
     return (
-      <div className="homepage">
-        <Header / >
-        <Button variant="contained" color="primary">
-          Start!
-        </Button>
-      </div>
+        <Grid container direction= "column"
+          justify= "space-evenly"
+          alignItems= "center">
+            <Grid item>
+              <img src= {Logo} alt = "Trivia Night" height= "450vw" width="auto"/>
+            </Grid>
+            <Grid item>
+              <MuiThemeProvider theme={theme}>
+                  <Button variant="contained" color="primary" size= "large" component={Link} to="/gameguest">
+                  Play as Guest
+                  </Button>
+                  <LoginButton/>
+              </MuiThemeProvider>
+            </Grid>
+        </Grid>
     );
   }
 }
