62e4dc61b9c9ffb033823a769d28f0910512d513
Niels-Christian Kielland <nielschristian@NielsChtiansMBP.localdomain>
2018-10-08 13:16:43 -0700

    changed the answer display to highlight answer in red and green: removed the alert.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,16 @@ import Button from '@material-ui/core/Button';
 
 
 class Home extends Component {
+
+  handleClick(){
+    this.props.history.push('/game');
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="homepage">
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={this.handleClick.bind(this)}>
           Start!
         </Button>
       </div>
